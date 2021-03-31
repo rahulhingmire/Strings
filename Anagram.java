b8f2b16caa42cf179881cd7f192a87653aa8f288
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-03-31 11:58:04 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,33 @@
+package string;
+
+public class annagram {
+	
+	public static void main(String[] args) {
+		String a="aab";
+		String b="aba";
+		boolean isAnagram=true;
+		int al[]=new int[256];
+		int bl[]=new int[256];
+		for(char c:a.toCharArray()) {
+			int index=(int) c;
+			al[index]++;
+		  }
+		for(char c:b.toCharArray()) {
+			int index=(int) c;
+			bl[index]++;
+		  }
+		for(int i=0;i<256;i++) {
+		if(al[i]!=bl[i]) {
+			isAnagram=false;
+			break;
+		}
+		}
+		if(isAnagram) {
+			System.out.println("Strings are Anagram");
+		}else {
+			System.out.println("Strings are not Anagram");
+		}
+				
+
+}
+}
