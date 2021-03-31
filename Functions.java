d96d987801bad0470e6fe8a317162662db062de3
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-03-31 11:57:14 +0530

    Create Functions.java

---
+++ Functions.java
@@ -0,0 +1,15 @@
+//Their are many functions some of them are used in code
+package string;
+
+public class Functions {
+	public static void main(String[] args) {
+		String a="rahul";
+		System.out.println(a.charAt(2));
+		System.out.println(a.length());
+		System.out.println(a.substring(3));
+		System.out.println(a.subSequence(1, 3));
+		System.out.println(a.contains(a));
+		
+
+}
+}
